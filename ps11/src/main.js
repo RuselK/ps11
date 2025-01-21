a7f7bdb48778ff16124a75b4aa6e2fa523cd1708
Ruslan <RuslanKirpichnikov@gmail.com>
2025-01-21 14:07:31 +0300

    complete

--- ps11/src/main.js
+++ ps11/src/main.js
@@ -22,23 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
  window.addEventListener("resize", handleResize)
 
- const particles = document.getElementById("particles")
- for (let i = 0; i < 50; i++) {
-   const particle = document.createElement("div")
-   particle.classList.add("absolute", "bg-white", "rounded-full", "opacity-50")
-   particle.style.width = `${Math.random() * 5 + 1}px`
-   particle.style.height = particle.style.width
-   particle.style.left = `${Math.random() * 100}%`
-   particle.style.top = `${Math.random() * 100}%`
-   particles.appendChild(particle)
- }
-
  const year = document.getElementById("year")
  const updateYear = () => {
    year.textContent = new Date().getFullYear()
  }
 
- 
+
  updateYear()
  handleResize()
 })
