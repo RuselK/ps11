f63d4f86982a121e519205df5f3e0e6658fd33df
Ruslan <RuslanKirpichnikov@gmail.com>
2025-02-09 11:25:17 +0300

    Add Yandex Metrika

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import { Header } from "@/components/landing/header"
 import { Footer } from "@/components/landing/footer"
 import { CookieBanner } from "@/components/cookieBanner"
+import { YMInitializer } from 'react-yandex-metrika';
 
 export default function MainLayout({
   children,
@@ -14,7 +15,7 @@ export default function MainLayout({
       <main>{children}</main>
       <Footer />
       <CookieBanner />
+      <YMInitializer accounts={[99630678]} />
     </>
   )
 }
-
